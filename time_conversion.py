b0f9eba32335115dff9cae60ce2c6eb9b5d78ed1
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-04-13 20:21:21 -0600

    Update time_conversion.py

--- time_conversion.py
+++ time_conversion.py
@@ -1,27 +1,26 @@
-# Python program to convert time
-# from 12 hour to 24 hour format
-  
 # Function to convert the date format
-def convert24(str1):
+def convert24(hour,min,denotion):
       
     # Checking if last two elements of time
     # is AM and first two elements are 12
-    if str1[-2:] == "AM" and str1[:2] == "12":
-        return "00" + str1[2:-2]
+    if denotion == "AM" and hour == "12":
+        return ('00:'+min)
           
     # remove the AM    
-    elif str1[-2:] == "AM":
-        return str1[:-2]
+    elif denotion == "AM":
+        return (hour+':'+min)
       
     # Checking if last two elements of time
     # is PM and first two elements are 12   
-    elif str1[-2:] == "PM" and str1[:2] == "12":
-        return str1[:-2]
+    elif denotion == "PM" and hour == "12":
+        return (hour+':'+min)
           
     else:
           
         # add 12 to hours and remove PM
-        return str(int(str1[:2]) + 12) + str1[2:8]
+        military = str(int(hour) + 12)
+        return (military+':'+min)
   
-# Driver Code        
-print(convert24("08:05:45 PM"))
+# Driver Code
+
+print (convert24('12','30','AM'))
