40ae9b7ac77d7822cb92d08beb6b6e0b8c1f822e
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-04-14 20:31:11 -0600

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -1,8 +1,8 @@
-print ("This program will solve multiplication probelms.")
+print ("This program will solve multiplication problems.")
 print ("Enter 0 for both terms to quit.")
 
-first = int(input("First number:"))
-second = int(input("Second number:"))
+first = int(input("First number: "))
+second = int(input("Second number: "))
 while first != 0 and second != 0:
 
     third = first * second
