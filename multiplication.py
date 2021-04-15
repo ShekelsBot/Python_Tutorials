395b36e9f151b47669bb717702ab0d00b92fa54b
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-04-14 20:23:31 -0600

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -3,8 +3,14 @@ print ("Enter 0 for both terms to quit.")
 
 first = int(input("First number: "))
 second = int(input("Second number: "))
+while first != 0 and second != 0:
 
-third = first * second
+    third = first * second
 
-print (first, " x " ,second, " = " ,third,)
-print ("Goodbye!")
+    print (first, " x " ,second, " = " ,third,)
+
+    first = int(input("First number: "))
+    second = int(input("Second number: "))
+
+if first == 0 and second == 0:
+    print ("Goodbye!")
