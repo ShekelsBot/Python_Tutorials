b3fc2280753a42cc50a334eb0456addf56ae57c1
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-04-14 20:08:23 -0600

    Add files via upload

---
+++ first_last_match.py
@@ -0,0 +1,12 @@
+def first_last_match(sentence):
+    length = len(sentence)
+    if len(sentence) == 0:
+        print ("False")
+    elif len(sentence) == 1:
+        print ("True")
+    elif sentence[0] == sentence[length-1]:
+        print ("SAME")
+    else:
+        print ("DIFF")
+first_last_match(sentence = input(""))
+
