8f63d395e5a9f4a90e29de9dc700ac67511644e5
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-04-21 20:28:54 -0600

    Update piglatin_2.py

--- piglatin_2.py
+++ piglatin_2.py
@@ -1,18 +1,23 @@
-def pig_it(text):
-    words = text.split(" ")
-    # Split sentence up
-    new_words = []
-    # Blank Array
-    for word in words:
-    # Parse Array
-        if word.isalpha():
-            new_word = word[1:] + word[0] + "ay"
-            # Re-arrange word and add ay
-            new_words.append(new_word)
-        else:
-            new_words.append(word)
-         
-         
-    return " ".join(new_words)
-    #Append new words together
-print(pig_it('cat'))
+string1=input("Enter a Sentence: ").strip()
+words=string1.split()
+l_words=string1.lower().split()
+pig_latin=[]
+ 
+for word in l_words:
+    if word[0] in 'aeiou':
+        pig_latin.append(word+'way')
+    else:
+        vowel_pos=0
+        for w in word:
+            if w in 'aeiou':
+                vowel_pos=word.index(w)
+                break
+            else:
+                pass
+        pig_latin.append(word[vowel_pos:]+word[:vowel_pos]+'ay')
+ 
+for i in range (0,len(words)-1):
+    if words[i].istitle():
+        pig_latin[i]=pig_latin[i].title()
+ 
+print("Pig Latin Version is: "+" ".join(pig_latin).strip())
