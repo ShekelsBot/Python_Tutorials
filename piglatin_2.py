1905bcea4546a9605bd71d8c46691df612d41b14
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-04-21 20:43:59 -0600

    Update piglatin_2.py

--- piglatin_2.py
+++ piglatin_2.py
@@ -1,27 +1,28 @@
 def pigLatin (string1):
-    words=string1.split()
-    l_words=string1.lower().split()
-    pig_latin=[]
+    words=string1.split() # Split word array into letters
+    l_words=string1.lower().split() # Make everything lowecase
+    pig_latin=[] # Make new empty array for pig latin transaltion
     
     for word in l_words:
-        if word[0] in 'aeiou':
-            pig_latin.append(word+'way')
+        if word[0] in 'aeiou': # Search array for letters AEIOU
+            pig_latin.append(word+'way') # Add WAY if found
         else:
-            vowel_pos=0
-            for w in word:
-                if w in 'aeiou':
-                    vowel_pos=word.index(w)
+            vowel_pos=0 # Counter
+            for w in word: # Go though array
+                if w in 'aeiou': # If vlaue matches
+                    vowel_pos=word.index(w) #Write index position
                     break
                 else:
                     pass
-            pig_latin.append(word[vowel_pos:]+word[:vowel_pos]+'ay')
+            pig_latin.append(word[vowel_pos:]+word[:vowel_pos]+'ay') #Convert word to piglatin
     
     for i in range (0,len(words)-1):
         if words[i].istitle():
             pig_latin[i]=pig_latin[i].title()
     
-    print("Pig Latin Version is: "+" ".join(pig_latin).strip())
+    print("Pig Latin Version is: "+" ".join(pig_latin))
 
 pigLatin("shark")
 pigLatin("Ant")
+pigLatin("Cat")
 
