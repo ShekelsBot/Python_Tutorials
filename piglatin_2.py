d32b5488028cca0eaacab858c2fdfa5b659d7b29
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-04-21 20:34:32 -0600

    Update piglatin_2.py

--- piglatin_2.py
+++ piglatin_2.py
@@ -1,23 +1,27 @@
-string1=input("Enter a Sentence: ").strip()
-words=string1.split()
-l_words=string1.lower().split()
-pig_latin=[]
- 
-for word in l_words:
-    if word[0] in 'aeiou':
-        pig_latin.append(word+'way')
-    else:
-        vowel_pos=0
-        for w in word:
-            if w in 'aeiou':
-                vowel_pos=word.index(w)
-                break
-            else:
-                pass
-        pig_latin.append(word[vowel_pos:]+word[:vowel_pos]+'ay')
- 
-for i in range (0,len(words)-1):
-    if words[i].istitle():
-        pig_latin[i]=pig_latin[i].title()
- 
-print("Pig Latin Version is: "+" ".join(pig_latin).strip())
+def pigLatin (string1):
+    words=string1.split()
+    l_words=string1.lower().split()
+    pig_latin=[]
+    
+    for word in l_words:
+        if word[0] in 'aeiou':
+            pig_latin.append(word+'way')
+        else:
+            vowel_pos=0
+            for w in word:
+                if w in 'aeiou':
+                    vowel_pos=word.index(w)
+                    break
+                else:
+                    pass
+            pig_latin.append(word[vowel_pos:]+word[:vowel_pos]+'ay')
+    
+    for i in range (0,len(words)-1):
+        if words[i].istitle():
+            pig_latin[i]=pig_latin[i].title()
+    
+    print("Pig Latin Version is: "+" ".join(pig_latin).strip())
+
+pigLatin("shark")
+pigLatin("Ant")
+
